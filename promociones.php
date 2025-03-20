22b457267d5de56fac17604de493db69c08edd14
Rene <al03045023@tecmilenio.mx>
2025-03-19 18:10:49 -0700

    PROMOCIONES Y DETALLES DEL PRODUCTO

---
+++ promociones.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Promociones - Xtreme Tactical</title>
+</head>
+<body>
+
+    <header>
+    <img src="img/LOGOXD.png" alt="LOGO">
+        <div>
+            <h1>XTREME TACTICAL AIR GUNS</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">INICIO</a></li>
+                <li><a href="cuchillos.php">CUCHILLOS</a></li>
+                <li><a href="armas.php">ARMAS DE AIRE</a></li>
+                <li><a href="equipamiento.php">EQUIPAMIENTO</a></li>
+                <li><a href="otros.php">OTROS</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div>
+            <h2>PROMOCIONES</h2>
+        </div>
+    </main>
+
+</body>
+</html>
